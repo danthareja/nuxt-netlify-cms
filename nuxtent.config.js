ec584be15381390043ae5adfd092881f350695e5
Dan Thareja <danthareja@gmail.com>
2018-04-15 11:39:54 -0600

    Configure nuxtent api

--- nuxtent.config.js
+++ nuxtent.config.js
@@ -8,5 +8,11 @@ module.exports = {
         'get'
       ]
     }]
-  ]
+  ],
+  api: {
+    baseURL: 'http://localhost:3000',
+    browserBaseURL: process.env.NODE_ENV === 'production'
+      ? 'https://nuxt-netlify-cms.netlify.com'
+      : ''
+  }
 }
